9abb8fea5b19b1310aca5cf427a508bca589bcd2
florlafuente <lafuente.florencia@gmail.com>
2018-05-12 12:40:03 -0300

    Add index and about routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './pages/home'
+import About from './pages/about'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Router>
+        <Fragment>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+        </Fragment>
+      </Router>
+    )
   }
 }
 
-export default App;
+export default App
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,8 @@
 import React from 'react'
 
-export default class Home extends React.Component {
-  render () {
-    return (
-      <div>
-        Soy Home
-      </div>
-    )
-  }
-}
+export default () => (
+  <div>
+    Soy home
+  </div>
+)
+
