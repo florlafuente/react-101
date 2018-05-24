02903b6eee97dd8708acb61c1ea24446a663b7a7
florlafuente <lafuente.florencia@gmail.com>
2018-05-24 19:32:40 -0300

    Fix about text

--- src/components/AboutText/index.js
+++ src/components/AboutText/index.js
@@ -4,8 +4,8 @@ import './index.css'
 export default () => (
   <div className='about-text-container'>
     <p className='about-text-paragraph'>Hi! I'm Flor. A visual artist and web developer from Buenos Aires.<br />
-    I currently work as a full stack javascript dev at Democracia en Red Foundation.
-    Our core project is DemocracyOS, but we also make implementations for different governments in Argentina.<br />
-    I also enjoy textile arts like embroidery, quilting, knitting. I like to keep my hands busy and to make cute stuff</p>
+    I currently work as a full stack javascript dev at <a href='http://www.democraciaenred.org' className='about-text-link' target='_blank'>Democracia en Red Foundation</a>.
+    Our core project is <a href='https://github.com/DemocracyOS/democracyos' className='about-text-link' target='_blank'>DemocracyOS</a>, but we also make implementations for different governments in Argentina. I use Javascript and several tools as React, Next, Node.js, Express and MongoDB on a daily basis.<br />
+    I also enjoy textile arts like embroidery, quilting, knitting. I like to keep my hands busy and make cute stuff.</p>
   </div>
 )
