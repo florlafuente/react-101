e765436ad3684f675de6f95e399b3e9bf29ee7e4
florlafuente <lafuente.florencia@gmail.com>
2018-05-24 19:33:49 -0300

    Fix errors

--- src/components/AboutText/index.js
+++ src/components/AboutText/index.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './index.css'
 
 export default () => (
   <div className='about-text-container'>
     <p className='about-text-paragraph'>Hi! I'm Flor. A visual artist and web developer from Buenos Aires.<br />
-    I currently work as a full stack javascript dev at <a href='http://www.democraciaenred.org' className='about-text-link' target='_blank'>Democracia en Red Foundation</a>.
-    Our core project is <a href='https://github.com/DemocracyOS/democracyos' className='about-text-link' target='_blank'>DemocracyOS</a>, but we also make implementations for different governments in Argentina. I use Javascript and several tools as React, Next, Node.js, Express and MongoDB on a daily basis.<br />
+    I currently work as a full stack javascript dev at <a href='http://www.democraciaenred.org' className='about-text-link' target='_blank' rel='noopener noreferrer'>Democracia en Red Foundation</a>.
+    Our core project is <a href='https://github.com/DemocracyOS/democracyos' className='about-text-link' target='_blank' rel='noopener noreferrer'>DemocracyOS</a>, but we also make implementations for different governments in Argentina. I use Javascript and several tools as React, Next, Node.js, Express and MongoDB on a daily basis.<br />
     I also enjoy textile arts like embroidery, quilting, knitting. I like to keep my hands busy and make cute stuff.</p>
   </div>
 )
