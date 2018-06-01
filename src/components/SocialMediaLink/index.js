9b2d614b98efcfe4501753792477dbae5fcbe956
florlafuente <lafuente.florencia@gmail.com>
2018-06-01 12:47:01 -0300

    SocialMediaLink styles. Closes #1

--- src/components/SocialMediaLink/index.js
+++ src/components/SocialMediaLink/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import './index.css'
 
 export default ({ icon, link}) => (
-  <a href={link} target='blank'>
+  <a href={link} target='blank' className='social-nav-link'>
     <span className={icon} />
   </a>
 )
